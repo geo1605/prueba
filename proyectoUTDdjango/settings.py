f574c80b5becfabb34705d917368348a6ab03394
geovanyArenas <145078589+geo1605@users.noreply.github.com>
2024-11-23 21:48:36 -0600

    Add files via upload

--- proyectoUTDdjango/settings.py
+++ proyectoUTDdjango/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+CSRF_TRUSTED_ORIGINS=['https://web-production-53f9.up.railway.app']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
